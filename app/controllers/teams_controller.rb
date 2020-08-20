e0fd0e259b4c21d13d99a94c2e3def85e187dc93
daisuke.okada <okada.daisuke@lmi.ne.jp>
2020-08-20 21:53:48 +0900

    create team_controller

---
+++ app/controllers/teams_controller.rb
@@ -0,0 +1,10 @@
+class TeamsController < ApplicationController
+  def new
+  end
+
+  def create
+  end
+
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  get 'teams/new'
+  get 'teams/create'
+  get 'teams/destroy'
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
