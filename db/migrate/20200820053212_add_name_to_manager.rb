75e7db3119ef04a6e8420bbe504a5eba2881fbc7
motoki-shirata <66302353+motoki-shirata@users.noreply.github.com>
2020-08-20 16:06:36 +0900

    Revert "Feature/create manager"

--- db/migrate/20200820053212_add_name_to_manager.rb
+++
@@ -1,5 +0,0 @@
-class AddNameToManager < ActiveRecord::Migration[6.0]
-  def change
-    add_column :managers, :name, :string
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,20 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_20_053212) do
-
-  create_table "managers", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
-    t.index ["email"], name: "index_managers_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_managers_on_reset_password_token", unique: true
-  end
+ActiveRecord::Schema.define(version: 2020_08_20_024506) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
