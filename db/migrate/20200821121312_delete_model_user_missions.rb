821690b7d0b89421dd255f7c7d9db69d8aa2b59e
daisuke.okada <okada.daisuke@lmi.ne.jp>
2020-08-21 21:52:40 +0900

    modify parent_task

--- db/migrate/20200821121312_delete_model_user_missions.rb
+++
@@ -1,5 +0,0 @@
-class DeleteModelUserMissions < ActiveRecord::Migration[6.0]
-  def change
-    drop_table :user_missions
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_21_003016) do
+ActiveRecord::Schema.define(version: 2020_08_21_124201) do
 
   create_table "bosses", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -55,21 +55,14 @@ ActiveRecord::Schema.define(version: 2020_08_21_003016) do
     t.string "explanation"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_parent_tasks_on_user_id"
   end
 
   create_table "teams", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-  end
-
-  create_table "user_missions", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.bigint "parent_task_id", null: false
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-    t.index ["parent_task_id"], name: "index_user_missions_on_parent_task_id"
-    t.index ["user_id"], name: "index_user_missions_on_user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci", force: :cascade do |t|
@@ -90,6 +83,5 @@ ActiveRecord::Schema.define(version: 2020_08_21_003016) do
   add_foreign_key "child_tasks", "parent_tasks"
   add_foreign_key "organizations", "bosses"
   add_foreign_key "organizations", "teams"
-  add_foreign_key "user_missions", "parent_tasks"
-  add_foreign_key "user_missions", "users"
+  add_foreign_key "parent_tasks", "users"
 end
