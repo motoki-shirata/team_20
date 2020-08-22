0795d164b9ad03ff2a8673d12888b1df4b0d46c2
daisuke.okada <okada.daisuke@lmi.ne.jp>
2020-08-22 16:51:56 +0900

    add child_task index to parent_tasks

--- app/controllers/parent_tasks_controller.rb
+++ app/controllers/parent_tasks_controller.rb
@@ -3,6 +3,7 @@ class ParentTasksController < ApplicationController
 
     def index
         @parent_tasks = ParentTask.all
+        @parent_task = ParentTask.new
     end
     def show
         @parent_task = ParentTask.find(params[:id])
