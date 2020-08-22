992edb21d9dbf24c702a62785cfcee070b592b81
daisuke.okada <okada.daisuke@lmi.ne.jp>
2020-08-22 15:29:19 +0900

    modify conflict

--- app/controllers/child_tasks_controller.rb
+++ app/controllers/child_tasks_controller.rb
@@ -1,40 +1,32 @@
 class ChildTasksController < ApplicationController
     before_action :authenticate_user!
 
-
     def index
-       @child_tasks = ChildTask.all 
+       @child_tasks = ChildTask.all
     end
     def show
-        @child_task = ChildTask.find(params[:id])
     end
     def new
+        @parent_task = ParentTask.find(params[:parent_task_id])
         @child_task = ChildTask.new
     end
     def create
-        @child_task = current_user.child_tasks.build(child_task_params)
+        @child_task = ChildTask.new(child_task_params)
+        @child_task.parent_task_id = params[:parent_task_id]
         if @child_task.save
             flash[:success] = 'タスクを作成しました'
-            redirect_to new_child_task
+            redirect_to parent_tasks_path
         end
     end
     def delete
-        
     end
     def edit
-        
     end
     def update
-        
     end
-    
-    
     private
 
     def child_task_params
-        params.require(:child_task).premit(:title, :child_deadline, :possibility, :result, :done, :explanation)
+        params.require(:child_task).permit(:title, :explanation, :child_deadline, :possibility)
     end
-    
-    
-    
 end
