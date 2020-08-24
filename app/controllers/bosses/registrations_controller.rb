404137d1ad3fcb054c4df396c74d7d86405d232e
motoki.shirata <shirata.motoki@lmi.ne.jp>
2020-08-24 16:47:45 +0900

    fix login-logout on user/boss

--- app/controllers/bosses/registrations_controller.rb
+++ app/controllers/bosses/registrations_controller.rb
@@ -51,12 +51,12 @@ class Bosses::RegistrationsController < Devise::RegistrationsController
   # end
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_sign_up_path_for(resource)
+    bosses_new_team_path
+  end
 
   # The path used after sign up for inactive accounts.
-  # def after_inactive_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_inactive_sign_up_path_for(resource)
+    bosses_new_team_path
+  end
 end
